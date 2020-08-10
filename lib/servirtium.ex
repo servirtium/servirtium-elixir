4cf5f0375e5e0ee437646aa32e29c163d4fc2c49
Josh Price <josh@alembic.com.au>
2020-08-10 19:07:52 +1000

    Record conn after function plug interaction

--- lib/servirtium.ex
+++ lib/servirtium.ex
@@ -2,27 +2,23 @@ defmodule Servirtium do
   import Plug.Conn
 
   @moduledoc """
-  Documentation for `ServirtiumElixir`.
+  Documentation for `Servirtium`.
   """
 
   @spec init(any) :: any
   def init(options) do
+    # check we have a plug delegate
     options
   end
 
-    @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ServirtiumElixir.hello()
-      :world
+  @doc """
 
   """
   @spec call(Plug.Conn.t(), any) :: Plug.Conn.t()
-  def call(conn, _opts) do
-    conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(200, "world")
+  def call(conn, opts) do
+    conn = opts[:plug].(conn, opts)
+
+    # record headers and bodies from request/response
+    assign(conn, :private, %{servirtium: %{conn: conn}})
   end
 end
--- test/servirtium_test.exs
+++ test/servirtium_test.exs
@@ -2,20 +2,37 @@ defmodule ServirtiumTest do
   use ExUnit.Case
   use Plug.Test
 
-  # doctest ServirtiumElixir
+  doctest Servirtium
 
-  @opts Servirtium.init([])
+  def hello_world_plug(conn, _) do
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(200, "world")
+  end
 
   test "returns hello world" do
     # Create a test connection
     conn = conn(:get, "/hello")
 
     # Invoke the plug
-    conn = Servirtium.call(conn, @opts)
+    conn = Servirtium.call(conn, plug: &hello_world_plug/2)
 
     # Assert the response and status
     assert conn.state == :sent
     assert conn.status == 200
     assert conn.resp_body == "world"
+  end
+
+  test "wraps plug and records the interaction with a function plug" do
+    # Create a test connection
+    conn = conn(:get, "/hello")
+
+    # Invoke the plug
+    conn = Servirtium.call(conn, plug: &hello_world_plug/2)
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.assigns.private.servirtium.conn.request_path == "/hello"
   end
 end
