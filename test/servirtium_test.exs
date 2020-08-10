14b03e47be7b0b99c03541a52e6a3da02e060d6a
Josh Price <josh@alembic.com.au>
2020-08-10 19:11:52 +1000

    Check response body in recording

--- test/servirtium_test.exs
+++ test/servirtium_test.exs
@@ -11,28 +11,21 @@ defmodule ServirtiumTest do
   end
 
   test "returns hello world" do
-    # Create a test connection
     conn = conn(:get, "/hello")
-
-    # Invoke the plug
     conn = Servirtium.call(conn, plug: &hello_world_plug/2)
 
-    # Assert the response and status
     assert conn.state == :sent
     assert conn.status == 200
     assert conn.resp_body == "world"
   end
 
   test "wraps plug and records the interaction with a function plug" do
-    # Create a test connection
     conn = conn(:get, "/hello")
-
-    # Invoke the plug
     conn = Servirtium.call(conn, plug: &hello_world_plug/2)
 
-    # Assert the response and status
     assert conn.state == :sent
     assert conn.status == 200
     assert conn.assigns.private.servirtium.conn.request_path == "/hello"
+    assert conn.assigns.private.servirtium.conn.resp_body == "world"
   end
 end
